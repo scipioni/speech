2f9218e3fc6afb5248d30b42b66eef1b27579fe0
Stefano Scipioni <scipio.it@gmail.com>
2017-03-08 09:39:05 +0100

    .

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 sudo apt install espeak
 sudo apt install portaudio19-dev python-dev swig libpulse-dev
 
+pip install pyaudio
 pip install speechrecognition
 pip install pyttsx
 pip install pocketsphinx
--- jarvis/test.py
+++ jarvis/test.py
@@ -2,7 +2,7 @@ import speech_recognition
 import pyttsx
 
 speech_engine = pyttsx.init('espeak') # see http://pyttsx.readthedocs.org/en/latest/engine.html#pyttsx.init
-speech_engine.setProperty('rate', 150)
+#speech_engine.setProperty('rate', 150)
 
 def speak(text):
     speech_engine.say(text)
@@ -11,7 +11,13 @@ def speak(text):
 recognizer = speech_recognition.Recognizer()
 
 def listen():
-    with speech_recognition.Microphone() as source:
+    microphone_index = 0
+    for i, name in enumerate(speech_recognition.Microphone.list_microphone_names()):
+        if name == "pulse":
+            microphone_index = i
+    #return "car"
+
+    with speech_recognition.Microphone(device_index=microphone_index) as source:
         recognizer.adjust_for_ambient_noise(source)
         print("Listening...")
         audio = recognizer.listen(source)
@@ -19,8 +25,9 @@ def listen():
 
     try:
         pass
-        #return recognizer.recognize_sphinx(audio)
-        return recognizer.recognize_google(audio)
+        text = recognizer.recognize_sphinx(audio, language = "en-US")
+        return text
+        #return recognizer.recognize_google(audio)
     except speech_recognition.UnknownValueError:
         print("Could not understand audio")
     except speech_recognition.RequestError as e:
@@ -28,6 +35,8 @@ def listen():
 
     return ""
 
-speak("Say something!")
-speak("I heard you say " + listen())
-speak("Say something!")
+
+while True:
+    print("text:",listen())
+#speak("I heard you say " + listen())
+#time.sleep(5)
